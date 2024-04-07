172497b034fcb19e797378b9ddc00732cbcc4c3a
nicholasmcnamra <nikomcnamara@gmail.com>
2024-04-07 12:48:54 -0400

    finished

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +13,45 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String [] tempArray = new String [array.length];
+        int newArrLength = 0;
+        for (int i = 0; i < this.array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < this.array.length; j++) {
+                if (this.array[i].equals(this.array[j])) {
+                    count++;
+                }
+            }
+            if (count < maxNumberOfDuplications) {
+                //need to create new array to store values that do not meet this condition
+                tempArray[newArrLength] = this.array[i];
+                newArrLength++;
+            }
+        }
+        String [] resultArray = Arrays.copyOf(tempArray, newArrLength);
+
+        return resultArray;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String [] tempArray = new String [array.length];
+        int newArrLength = 0;
+        for (int i = 0; i < this.array.length; i++) {
+            int count = 0;
+            for (int j = 0; j < this.array.length; j++) {
+                if (this.array[i].equals(this.array[j])) {
+                    count++;
+                }
+            }
+            if (count != exactNumberOfDuplications) {
+                //need to create new array to store values that do not meet this condition
+                tempArray[newArrLength] = this.array[i];
+                newArrLength++;
+            }
+        }
+        String [] resultArray = Arrays.copyOf(tempArray, newArrLength);
+
+        return resultArray;
     }
 }
